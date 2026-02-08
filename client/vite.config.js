24ce9e96ffe86d64f6051b1f711074072911290f
Mohammed Faraz <110233150+TheJinn@users.noreply.github.com>
2026-02-08 23:24:39 +0530

    api fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    strictPort: true,
     proxy: {
-      '/api': 'http://localhost:5000',
-      '/uploads': 'http://localhost:5000'
+      '/api': process.env.VITE_API_URL,
+      '/uploads': process.env.VITE_API_URL
     }
   }
 });
